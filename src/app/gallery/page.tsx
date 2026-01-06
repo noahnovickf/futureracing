55d1898e05fa8da0c46727074b7c0b6adf20bd70
noahnovickf <nnovickfaille@gmail.com>
2026-01-06 13:44:26 -0800

    add gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,4 +1,26 @@
+import IconContainer from '../IconContainer';
+import StaticWindow from '../components/window/StaticWindow';
+
 const Gallery = () => {
-  return <div>gallery</div>;
+  return (
+    <div>
+      <IconContainer />
+      <StaticWindow
+        header="future_cycling/gallery"
+        style={{
+          position: 'absolute',
+          width: '80%',
+          left: '10%',
+          top: '5%',
+        }}
+      >
+        <script src="https://elfsightcdn.com/platform.js" async></script>
+        <div
+          className="elfsight-app-1b2d3255-0249-453d-b247-53f10ead7618"
+          data-elfsight-app-lazy
+        ></div>
+      </StaticWindow>
+    </div>
+  );
 };
 export default Gallery;
